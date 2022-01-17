1390e003a10f0aa1e738eb831c290c392c4ee799
HedyLuo <1570968754@qq.com>
2022-01-17 22:52:36 +0800

    提交测试套件

---
+++ Chapter5/src/main/java/com/course/testng/suite/PayTest.java
@@ -0,0 +1,11 @@
+package com.course.testng.suite;
+
+import org.testng.annotations.Test;
+
+public class PayTest {
+
+    @Test
+    public void payZhifubao(){
+        System.out.println("pay for zhifubao sussess!");
+    }
+}
