6f48774c7797edd7a61d462f48e1b7e92c02799f
han <luoxuehong.lxh@alibaba-inc.com>
2024-03-16 23:31:46 +0800

    test

---
+++ Practice/src/main/java/leetcode/editor/cn/P1_TwoSum.java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+/**
+ * 2024-03-16 23:29:16
+ * 两数之和
+ * 编号：[1]
+ * @author saint
+ */
+public class P1_TwoSum{
+    public static void main(String[] args) {
+        Solution solution = new P1_TwoSum().new Solution();      
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        return nums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
